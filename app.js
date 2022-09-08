334f4bae2e9bfb87d517558d8c4a999b4bc30300
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-09-08 05:27:26 -0500

    added mandatory question submits

--- app.js
+++ app.js
@@ -8,12 +8,41 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else{
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else{
+            console.log('Please enter a description!');
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -24,7 +53,15 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else{
+            console.log('Please enter the GitHub link!');
+            return false;
+          }
+        }
       },
       {
         type: 'confirm',
